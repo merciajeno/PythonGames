d0d71854a32e23bd8949db96ac45fdb8e5bf61fc
Mercia Jeno <mercia.jeno@gmail.com>
2026-04-22 21:49:18 +0530

    changed dino shape

--- GoogleDinosaurGame/main.py
+++ GoogleDinosaurGame/main.py
@@ -1,13 +1,19 @@
 import tkinter as tk
+from PIL import Image,ImageTk
 
 root = tk.Tk()
 canvas = tk.Canvas(root, width=400, height=300)
 canvas.pack()
 
 # Load the image
-img = tk.PhotoImage(file="dino.png")
+img = Image.open("dino.png")
+new_width = 50
+new_height = 50
+resized_img = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
 
+#  Convert to Tkinter-compatible photo
+tk_img = ImageTk.PhotoImage(resized_img)
 # Add image to canvas (x, y coordinates)
-canvas.create_image(20, 20, anchor="nw", image=img)
+canvas.create_image(0, 100, anchor="nw", image=tk_img)
 
 root.mainloop()
