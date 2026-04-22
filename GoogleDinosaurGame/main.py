27a531efdbe8fa0d37afc99a7fa0c05f0ea53b60
Mercia Jeno <mercia.jeno@gmail.com>
2026-04-22 17:11:10 +0530

    added main

---
+++ GoogleDinosaurGame/main.py
@@ -0,0 +1,14 @@
+from tkinter import Tk, Canvas
+
+root = Tk()
+# Initialize canvas
+canvas = Canvas(root, width=400, height=200, bg="white")
+canvas.pack()
+
+# Draw a red line
+canvas.create_line(0, 0, 400, 200, fill="red")
+
+# Draw a blue rectangle
+canvas.create_rectangle(50, 50, 150, 150, fill="blue")
+
+root.mainloop()
