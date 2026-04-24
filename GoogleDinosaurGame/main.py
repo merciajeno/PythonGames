239dd6646496c0071c9ce2772d311b6fc4d6f5a7
Mercia Jeno <mercia.jeno@gmail.com>
2026-04-24 17:41:23 +0530

    made dinosaur move along x-direction

--- GoogleDinosaurGame/main.py
+++ GoogleDinosaurGame/main.py
@@ -11,9 +11,15 @@ new_width = 50
 new_height = 50
 resized_img = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
 
+def move_dino():
+    global x_speed, y_speed
+    canvas.move(dino,x_speed,0)
+    x_speed = x_speed+10
+    canvas.after(500, move_dino)
 #  Convert to Tkinter-compatible photo
 tk_img = ImageTk.PhotoImage(resized_img)
 # Add image to canvas (x, y coordinates)
-canvas.create_image(0, 100, anchor="nw", image=tk_img)
-
+dino = canvas.create_image(0, 100, anchor="nw", image=tk_img)
+x_speed = 0
+move_dino()
 root.mainloop()
