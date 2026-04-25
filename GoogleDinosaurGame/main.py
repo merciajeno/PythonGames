59902b8ec006c89e595f8cc7210d17b7918e9a87
Mercia Jeno <mercia.jeno@gmail.com>
2026-04-25 17:48:35 +0530

    created infinite line

--- GoogleDinosaurGame/main.py
+++ GoogleDinosaurGame/main.py
@@ -12,14 +12,24 @@ new_height = 50
 resized_img = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
 
 def move_dino():
-    global x_speed, y_speed
-    canvas.move(dino,x_speed,0)
-    x_speed = x_speed+10
-    canvas.after(500, move_dino)
+    canvas.move(ground1,-5,0)
+    canvas.move(ground2,-5,0)
+    x1 = canvas.coords(ground1)
+    x2 = canvas.coords(ground2)
+    if x1[2] < 0:
+        canvas.coords(ground1, x2[2], x2[3], x2[2] + 400, x2[3])
+    if x2[2] < 0:
+        canvas.coords(ground2, x1[2], x1[3], x1[2] + 400, x1[3])
+    print(x1)
+    print(x2)
+    canvas.after(30,move_dino)
+
 #  Convert to Tkinter-compatible photo
 tk_img = ImageTk.PhotoImage(resized_img)
+ground_y = 150
+ground1 = canvas.create_line(0,ground_y,400,ground_y,width=3)
+ground2 = canvas.create_line(400,ground_y,800,ground_y,width=3)
 # Add image to canvas (x, y coordinates)
-dino = canvas.create_image(0, 100, anchor="nw", image=tk_img)
-x_speed = 0
+dino = canvas.create_image(0, ground_y-50, anchor="nw", image=tk_img)
 move_dino()
 root.mainloop()
